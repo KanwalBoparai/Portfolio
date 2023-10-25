9eb2415b759742969b7c503300086adc28945da9
KanwalBoparai <133400024+KanwalBoparai@users.noreply.github.com>
2023-10-24 23:50:59 -0400

    add stars and mars

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Stars } from '@react-three/drei';
+import * as THREE from 'three';
+import earthTexture from './static/Mars.webp'; // Replace with the actual path to your earth texture image
+
+function CustomSphere() {
+  return (
+    <mesh>
+      <sphereGeometry args={[5, 32, 32]} attach="geometry" />
+      <meshLambertMaterial attach="material" map={new THREE.TextureLoader().load(earthTexture)} />
+    </mesh>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [10, -40, 0] }}
+    style={{ background: "#000"}}
+    dpr={Math.max(window.devicePixelRatio, 2)}
+    >
+      <Stars />
+      <OrbitControls />
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 15, 10]} angle={0.3} />
+      <CustomSphere />
+    </Canvas>
   );
 }
 
