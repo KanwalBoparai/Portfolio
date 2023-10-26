49be10db7b67da9b262b3d43fa71ce57f4328709
KanwalBoparai <133400024+KanwalBoparai@users.noreply.github.com>
2023-10-26 00:20:34 -0400

    mars done wit gsap

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
+import React, { useEffect, useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls, Stars } from '@react-three/drei';
 import * as THREE from 'three';
-import earthTexture from './Mars.webp'; // Replace with the actual path to your earth texture image
+import earthTexture from './mars1.jpg'; // Adjust the path to your earth texture image
+import gsap from 'gsap';
 
 function CustomSphere() {
+  const cameraRef = useRef(null);
+  useEffect(() => {
+    if (!!cameraRef.current) {
+      gsap.to(cameraRef.current.position, {
+        z: -60,
+        duration: 3
+      });
+    }
+  }, [cameraRef.current]);
+
   return (
-    <mesh>
-      <sphereGeometry args={[5, 32, 32]} attach="geometry" />
-      <meshLambertMaterial attach="material" map={new THREE.TextureLoader().load(earthTexture)} />
-    </mesh>
+    <>
+      <Stars />
+      <ambientLight intensity={0.5} />
+      <mesh ref={cameraRef}>
+        <sphereGeometry args={[5, 32, 32]} attach="geometry" />
+        <meshLambertMaterial attach="material" map={new THREE.TextureLoader().load(earthTexture)} />
+      </mesh>
+    </>
   );
 }
 
 function App() {
   return (
-    <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [10, -40, 0] }}
-    style={{ background: "#000"}}
-    dpr={Math.max(window.devicePixelRatio, 2)}
+    <>
+    <Canvas
+      style={{ background: "#000" }}
+      camera={{ fov: 75, near: 0.1, far: 1000 }}
+      dpr={Math.max(window.devicePixelRatio, 2)}
     >
-      <Stars />
       <OrbitControls />
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 15, 10]} angle={0.3} />
       <CustomSphere />
     </Canvas>
+    </>
   );
 }
 
